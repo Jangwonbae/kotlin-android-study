741366a7543fca4a1153d58d68e9936e571aef1d
wbjang <wonbae623@naver.com>
2026-04-01 14:01:31 +0900

    Amphibian App에 Hilt 적용해보기 (Retrofit)

--- coroutines-rest-coil-codelab-practice/src/main/java/com/wbjang/coroutines_rest_coil_codelab_practice/AmphibiansApplication.kt
+++ coroutines-rest-coil-codelab-practice/src/main/java/com/wbjang/coroutines_rest_coil_codelab_practice/AmphibiansApplication.kt
@@ -1,15 +1,7 @@
 package com.wbjang.coroutines_rest_coil_codelab_practice
 
 import android.app.Application
-import com.wbjang.coroutines_rest_coil_codelab_practice.data.AppContainer
-import com.wbjang.coroutines_rest_coil_codelab_practice.data.DefaultAppContainer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AmphibiansApplication : Application() {
-//    lateinit var container: AppContainer
-//    override fun onCreate() {
-//        super.onCreate()
-//        container = DefaultAppContainer()
-//    }
-}
+class AmphibiansApplication : Application()
